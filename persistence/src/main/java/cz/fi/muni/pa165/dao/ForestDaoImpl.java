61c9537777f2613f5792b2caa652f3e21c2fb625
Erik Macej <erikmacej15@gmail.com>
2016-10-24 00:08:35 +0200

    Configuration of spring and hibernate,ForestDao,ForestEntity

---
+++ persistence/src/main/java/cz/fi/muni/pa165/dao/ForestDaoImpl.java
@@ -0,0 +1,60 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.entity.Forest;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+/**
+ * Created by Erik Macej 433744 , on 23.10.16.
+ *
+ * @author Erik Macej 433744
+ */
+
+@Repository
+@Transactional
+public class ForestDaoImpl implements ForestDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    public void create(Forest forest) {
+        em.persist(forest);
+    }
+
+    @Override
+    public void edit(Forest forest) {
+        em.merge(forest);
+    }
+
+    @Override
+    public Forest findById(Long id) {
+        return em.find(Forest.class,id);
+    }
+
+    @Override
+    public void delete(Forest forest) throws IllegalArgumentException {
+        em.remove(forest);
+    }
+
+    @Override
+    public List<Forest> findAll() {
+        return em.createQuery("select f from Forest f"
+                , Forest.class).getResultList();
+    }
+
+    @Override
+    public Forest findByName(String name) {
+        try {
+            return em.createQuery("select f from Forest f where name = :name",
+                            Forest.class).setParameter("name", name).getSingleResult();
+        } catch (NoResultException nrf) {
+            return null;
+        }
+    }
+}
