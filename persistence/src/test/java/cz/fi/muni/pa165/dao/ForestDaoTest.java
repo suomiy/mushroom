38fed6257d51f50fbddaf73e8e06dc486b400772
Erik Macej <erikmacej15@gmail.com>
2016-10-31 03:46:38 +0100

    Adding ForestDaoTest,MushroomCountDaoTest

--- persistence/src/test/java/cz/fi/muni/pa165/dao/ForestDaoTest.java
+++ persistence/src/test/java/cz/fi/muni/pa165/dao/ForestDaoTest.java
@@ -2,10 +2,13 @@ package cz.fi.muni.pa165.dao;
 
 import cz.fi.muni.pa165.PersistenceApplicationContext;
 import cz.fi.muni.pa165.entity.Forest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,7 @@ import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 
 /**
@@ -34,17 +38,103 @@ public class ForestDaoTest extends AbstractTestNGSpringContextTests {
     @PersistenceContext
     private EntityManager em;
 
+    private Forest forest1;
+    private Forest forest2;
+
+    @BeforeMethod
+    public void createForest(){
+        forest1 = new Forest();
+        forest2 = new Forest();
+
+        forest1.setName("Forest1");
+        forest1.setLocalityDescription("locality1");
+        forest2.setName("Forest2");
+        forest2.setLocalityDescription("locality2");
+
+        forestDao.create(forest1);
+        forestDao.create(forest2);
+    }
+
     @Test
     public void createForestTest(){
 
-        Forest forest = new Forest();
-        forest.setName("Dark forest");
+        Forest forestForCreate = new Forest();
+        forestForCreate.setName("ForestForCreate");
 
-        forestDao.create(forest);
+        forestDao.create(forestForCreate);
+        Forest found = forestDao.findById(forestForCreate.getId());
 
-        Forest loadedForest = forestDao.findById(forest.getId());
-
-        Assert.assertEquals(loadedForest.getId(),forest.getId());
-        Assert.assertEquals(loadedForest.getName(),forest.getName());
+        Assert.assertEquals(found.getName(),forestForCreate.getName());
     }
+
+    @Test(expectedExceptions = JpaSystemException.class)
+    public void createForestWithNullName(){
+        Forest forest = new Forest();
+        forest.setName(null);
+        forestDao.create(forest);
+    }
+
+    @Test(expectedExceptions = DataAccessException.class)
+    public void createTwoForestWithSameName(){
+        Forest anotherForest = new Forest();
+        anotherForest.setName("Forest1");
+        forestDao.create(anotherForest);
+    }
+
+    @Test
+    public void findAllForestsTest(){
+        List<Forest> loadedForests = forestDao.findAll();
+        Assert.assertEquals(loadedForests.size(),2);
+
+        Forest assertForest1 = new Forest();
+        Forest assertForest2 = new Forest();
+
+        assertForest1.setName(forest1.getName());
+        assertForest1.setLocalityDescription(forest1.getLocalityDescription());
+        assertForest2.setName(forest2.getName());
+        assertForest2.setLocalityDescription(forest2.getLocalityDescription());
+
+        Assert.assertTrue(loadedForests.contains(assertForest1));
+        Assert.assertTrue(loadedForests.contains(assertForest2));
+    }
+
+    @Test
+    public void findForestByIdTest(){
+        Forest found = forestDao.findById(forest1.getId());
+
+        Assert.assertEquals(found.getName(),forest1.getName());
+        Assert.assertEquals(found.getLocalityDescription(),
+                forest1.getLocalityDescription());
+    }
+
+    @Test
+    public void deleteForestTest(){
+        forestDao.delete(forest1);
+        Assert.assertNull(forestDao.findById(forest1.getId()));
+    }
+
+    @Test
+    public void editForestTest() {
+        Forest editedForest = new Forest();
+        editedForest.setId(forest1.getId());
+        editedForest.setName("Dark Forest");
+        editedForest.setLocalityDescription("Dark locality");
+
+        forestDao.edit(editedForest);
+
+        Forest loadedForest = forestDao.findById(forest1.getId());
+
+        Assert.assertEquals(loadedForest.getName(),editedForest.getName());
+        Assert.assertEquals(loadedForest.getLocalityDescription(),
+                editedForest.getLocalityDescription());
+    }
+
+    @Test
+    public void findForestByNameTest(){
+        Forest found = forestDao.findByName("Forest1");
+        Assert.assertEquals(found.getName(),forest1.getName());
+        Assert.assertEquals(found.getLocalityDescription(),
+                forest1.getLocalityDescription());
+    }
+
 }
---
+++ persistence/src/test/java/cz/fi/muni/pa165/dao/MushroomCountDaoTest.java
@@ -0,0 +1,142 @@
+package cz.fi.muni.pa165.dao;
+
+import cz.fi.muni.pa165.PersistenceApplicationContext;
+import cz.fi.muni.pa165.entity.*;
+import cz.fi.muni.pa165.enums.MushroomType;
+import cz.fi.muni.pa165.enums.Rank;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.Month;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Erik Macej 433744 , on 31.10.16.
+ *
+ * @author Erik Macej 433744
+ */
+
+
+@ContextConfiguration(classes = PersistenceApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
+public class MushroomCountDaoTest extends AbstractTestNGSpringContextTests {
+
+    @Inject
+    private ForestDao forestDao;
+
+    @Inject
+    private HunterDao hunterDao;
+
+    @Inject
+    private MushroomDao mushroomDao;
+
+    @Inject
+    private VisitDao visitDao;
+
+    @Inject
+    private MushroomCountDao mushroomCountDao;
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private Forest forest;
+    private Hunter hunter;
+    private Mushroom mushroom1;
+    private Mushroom mushroom2;
+    private Visit visit;
+    private MushroomCount mushroomCount1;
+    private MushroomCount mushroomCount2;
+
+    @BeforeMethod
+    public void setUp(){
+        prepareForest();
+        prepareHunter();
+        prepareMushroom();
+        prepareMushroomCount();
+        prepareVisit();
+    }
+
+    private void prepareForest(){
+        forest = new Forest();
+        forest.setName("Forest");
+        forestDao.create(forest);
+    }
+
+    private void prepareHunter(){
+        hunter = new Hunter();
+        hunter.setFirstName("Jozef");
+        hunter.setSurname("Straka");
+        hunter.setNick("Straka");
+        hunter.setEmail("straka@gmail.com");
+        hunter.setPasswordHash("123456");
+        hunter.setRank(Rank.GURU);
+
+        hunterDao.create(hunter);
+    }
+
+    private void prepareMushroom(){
+        mushroom1 = new Mushroom();
+        mushroom1.setName("Dubak");
+        mushroom1.setType(MushroomType.EDIBLE);
+        mushroom1.setFromDate(Month.JANUARY);
+        mushroom1.setToDate(Month.FEBRUARY);
+
+        mushroom2 = new Mushroom();
+        mushroom2.setName("Kozak");
+        mushroom2.setType(MushroomType.EDIBLE);
+        mushroom2.setFromDate(Month.JANUARY);
+        mushroom2.setToDate(Month.FEBRUARY);
+
+        mushroomDao.create(mushroom1);
+        mushroomDao.create(mushroom2);
+    }
+
+    private void prepareMushroomCount(){
+        mushroomCount1 = new MushroomCount();
+        mushroomCount1.setMushroom(mushroom1);
+        mushroomCount1.setCount(6);
+
+        mushroomCount2 = new MushroomCount();
+        mushroomCount2.setMushroom(mushroom2);
+        mushroomCount2.setCount(5);
+    }
+
+    private void prepareVisit(){
+        visit = new Visit();
+        visit.setDate(new Date(1,1,1994));
+        visit.setForest(forest);
+        visit.setHunter(hunter);
+        visit.addMushroomCount(mushroomCount1);
+        visit.addMushroomCount(mushroomCount2);
+
+        visitDao.create(visit);
+    }
+
+    @Test
+    public void findAllMushroomCountsTest(){
+        List<MushroomCount> found = mushroomCountDao.findAll();
+
+        Assert.assertEquals(found.size(),2);
+        Assert.assertTrue(found.contains(mushroomCount1));
+        Assert.assertTrue(found.contains(mushroomCount2));
+    }
+
+    @Test
+    public void findMushroomCountTest(){
+        MushroomCount found = mushroomCountDao.findById(mushroomCount1.getId());
+        Assert.assertEquals(mushroomCount1,found);
+    }
+
+
+}
