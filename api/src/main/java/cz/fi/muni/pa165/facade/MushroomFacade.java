8cdd862f4073bc8e23cb02f99bc3ec551cc9d767
Michal Kysilko <michal@debian>
2016-11-23 19:34:50 +0100

    mushroom service, facade, dto

---
+++ api/src/main/java/cz/fi/muni/pa165/facade/MushroomFacade.java
@@ -0,0 +1,7 @@
+package cz.fi.muni.pa165.facade;
+
+/**
+ * Created by michal on 11/23/16.
+ */
+public class MushroomFacade {
+}
---
+++ service/src/main/java/cz/fi/muni/pa165/service/MushroomServiceImpl.java
@@ -0,0 +1,7 @@
+package cz.fi.muni.pa165.service;
+
+/**
+ * Created by michal on 11/23/16.
+ */
+public class MushroomServiceImpl {
+}
